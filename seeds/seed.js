9c71f95a35c9006dd3a5ee192b8b670260b62bc9
Headwall Forest <headwallforest27@gmail.com>
2022-10-12 21:30:23 -0400

    Adding files to models and seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const recipe of recipeData) {
+    await Recipe.create({
+      ...recipe,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
