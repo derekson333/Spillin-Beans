cf3d15bc26ca944f3cfb97435244d7c21ae5bdbc
Headwall Forest <headwallforest27@gmail.com>
2022-10-17 19:12:28 -0400

    Updated seeds + associations

--- models/index.js
+++ models/index.js
@@ -1,13 +1,30 @@
 const User = require('./User');
 const Recipe = require('./Recipe');
+const Ingredient = require('./Ingredient')
+const Instruction = require('./Instruction')
 
 User.hasMany(Recipe, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+Recipe.hasMany(Ingredient, {
+});
+
+Ingredient.belongsToMany(Recipe, {
+
+})
+
+Recipe.hasMany(Instruction, {
+});
+
+Instruction.belongsToMany(Recipe, {
+  
+})
+
+
 Recipe.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Recipe };
+module.exports = { User, Recipe, Ingredient, Instruction };
