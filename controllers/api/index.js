e3c896bb0db0b78a39145d4fc40852f2f148ca72
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-17 18:48:47 -0400

    create controller for the api routes

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const recipeRoutes = require('./recipeRoutes');
+const ingredientRoutes = require('./ingredientRoutes');
+
+router.use('/users', userRoutes);
+router.use('/recipes', recipeRoutes);
+router.use('/ingredients', ingredientRoutes);
+
+module.exports = router;
