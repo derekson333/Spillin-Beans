9ab3c6cafe30658234b595adde8012bba61d3694
derekson3333 <108297576+derekson333@users.noreply.github.com>
2022-10-20 17:48:06 -0400

    whoops missed 2nd entry

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_MARIA_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
