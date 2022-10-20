e588451a5ba402f1254888be8b487d942b4aa1f3
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-19 20:33:33 -0400

    open a new express session on signup

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -20,11 +20,16 @@ router.get('/', async (req, res) => {
 // POST route to add a single user
 router.post('/', async (req, res) => {
     try {
-        const userData = await User.create(body.req)
+        const userData = await User.create(req.body)
 
-        res.status(200).json(userData);
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
     } catch (err) {
-        res.status(500).json(err);
+        res.status(400).json(err);
     };
 });
 
