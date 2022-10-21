9ab976e59902e634272f1d2451e01f0ee787b7f2
derekson3333 <108297576+derekson333@users.noreply.github.com>
2022-10-21 18:15:47 -0400

    Basic image.js file;  loads on page open presently

---
+++ public/js/image.js
@@ -0,0 +1,23 @@
+// import * as filestack from 'filestack-js';
+// const client = filestack.init('A2Cms4wvRRnqHepwihpV0z');
+
+// client.picker().open();
+const apikey = 'A2Cms4wvRRnqHepwihpV0z';
+const client = filestack.init(apikey);
+const options = {
+  maxFiles: 1,
+  uploadInBackground: false,
+  onOpen: () => console.log('opened!'),
+  onUploadDone: (res) => console.log(res),
+};
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    client.picker(options).open();
+  });
+  
+
+
+// needs to go into handlebars page where this is implemented
+{/* <script src="//static.filestackapi.com/filestack-js/3.x.x/filestack.min.js"></script>
+<script src="/js/image.js"></script> */}
