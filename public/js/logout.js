1ff03f4f1be503c16f5f8343761ee5338e9afd5f
Paul Pinard <paul.k.pinard@gmail.com>
2022-10-21 15:34:06 -0400

    move the event listener out of the function

--- public/js/logout.js
+++ public/js/logout.js
@@ -13,5 +13,5 @@ const logout = async () => {
     }
   };
   
-  document.querySelector('#logout').addEventListener('click', logout);
+document.querySelector('#logout').addEventListener('click', logout);
   
