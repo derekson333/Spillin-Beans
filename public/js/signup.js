f43430f5bd41bb6f185d4a6a1f7085304af3fc7b
derekson3333 <108297576+derekson333@users.noreply.github.com>
2022-10-20 23:51:33 -0400

    Made same changes to signup; button isn't firing

--- public/js/signup.js
+++ public/js/signup.js
@@ -29,5 +29,5 @@ const signupFormHandler = async (event) => {
 };
 
 document
-    .querySelector('.signup-form')
+    // .querySelector('.signup-form')
     .addEventListener('submit', signupFormHandler);
