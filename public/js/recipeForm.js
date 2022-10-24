9349d86b284226192f51378706b17b64f68246a5
derekson3333 <108297576+derekson333@users.noreply.github.com>
2022-10-23 20:37:02 -0400

    Reverted recipe form before pull request

--- public/js/recipeForm.js
+++ public/js/recipeForm.js
@@ -9,23 +9,24 @@ const newFormHandler = async (event) => {
     if (name  && description) {
       const response = await fetch(`/api/recipes`, {
         method: 'POST',
-        body: JSON.stringify({ name, description, prep_time, cook_time, ingredients, instructions, img }), headers: {
+        body: JSON.stringify({ name,  description, prep_time, cook_time, ingredients, instructions }),
+        headers: {
           'Content-Type': 'application/json',
         },
       });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to create recipe');
-      }
-    }
-  };
-  
-  // Function that handles deleting a recipe
-  
  
-  document
-    .querySelector('.new-recipe-form')
-    .addEventListener('submit', newFormHandler);
-  
+     if (response.ok) {
+       document.location.replace('/');
+     } else {
+       alert('Failed to create recipe');
+     }
+   }
+ };
+ 
+ // Function that handles deleting a recipe
+ 
+
+ document
+   .querySelector('.new-recipe-form')
+   .addEventListener('submit', newFormHandler);
+ 
