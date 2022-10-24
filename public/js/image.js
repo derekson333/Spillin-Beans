83aafbcaa0bdf71818c31a9979613ca02047f109
derekson3333 <108297576+derekson333@users.noreply.github.com>
2022-10-23 21:47:36 -0400

    Setup image picker to save URL to variable

--- public/js/image.js
+++ public/js/image.js
@@ -1,12 +1,12 @@
 const uploadButton = document.getElementById('upload')
-// API setup to use filepicker
+// API setup to use filepicker and store image URL
 const apikey = 'A2Cms4wvRRnqHepwihpV0z';
 const client = filestack.init(apikey);
 const options = {
   maxFiles: 1,
   uploadInBackground: false,
   onOpen: () => console.log('opened!'),
-  onUploadDone: (res) => console.log(res.filesUploaded[0].url),
+  onUploadDone: (res) => imgUrl = res.filesUploaded[0].url
 };
 
 // Event listener on upload button to open file picker
